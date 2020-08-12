35164775104f5b167d1846796c28f3564a7b6d29
roundcornerofficial <69595088+roundcornerofficial@users.noreply.github.com>
2020-08-12 21:58:00 +0530

    v1-portfolio

---
+++ index.js
@@ -0,0 +1,55 @@
+$(".navbar .navbar-brand").hover(
+  function(){
+    $(".navbar .navbar-brand span").animate({paddingLeft: "10px"});
+  }, function(){
+    $(".navbar .navbar-brand span").animate({paddingLeft: "-10px"});
+  }
+)
+
+//// Scroll Animation ////
+
+
+$('.home-text').waypoint(function() {
+  $(".home-text h1").addClass("animate__animated animate__fadeInDown");
+  $(".home-text p, .home-btn button").addClass("animate__animated animate__fadeInUp");
+  $(".big-logo, .main-logo").addClass("animate__animated animate__fadeIn animate__slower");
+}, {
+  offset: '60%'
+});
+
+$(".project").waypoint(function(){
+  $(".project-set .image").addClass("animate__animated animate__fadeInLeft");
+  $(".project-set .detail").addClass("animate__animated animate__fadeInRight");
+  $("#sidebar .logo-div").addClass("animate__animated animate__fadeInLeft");
+  $("#sidebar .sidebar").addClass("animate__animated animate__fadeInLeft");
+},{
+  offset: "50%"
+});
+
+$("#about").waypoint(function(){
+  $("#about .card1").addClass("animate__animated animate__fadeInUp");
+},{
+  offset: "60%"
+});
+
+$("#project").waypoint(function(){
+  $("#sidebar .logo-div").removeClass("animate__animated animate__fadeInLeft");
+  $("#sidebar .sidebar").removeClass("animate__animated animate__fadeInLeft");
+},{
+  offset: "100%"
+});
+
+
+///// Smooth Page Scroll /////
+
+$(".scroll").click(function(e){
+  e.preventDefault();
+  $("body,html").animate({
+    scrollTop: $(this.hash).offset().top
+  },1000);
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggler:visible').click();
+});
